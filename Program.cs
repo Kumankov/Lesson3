6a8a08cb42a90d96135d116e9ade63c92642a8d6
Ilya <kumankov@gmail.ru>
2022-11-22 23:23:41 +0300

    22

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 //14212 -> нет
 //12821 -> да
 //23432 -> да
-//Task19();
+Task19();
 void Task19 ()
 {
 Console.WriteLine("Введите пятизначное число для задачи 19:");
@@ -19,27 +19,10 @@ else
     }
 }
 
-//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
-//3 -> 1, 8, 27
-//5 -> 1, 8, 27, 64, 125
-//Task23 ();
-void Task23 ()
-{
-Console.WriteLine("Введите число N для задачи 23:");
-int N = int.Parse(Console.ReadLine());
-for (int i=1;i<=N;i++)
-    {
-    if (i==N)
-    Console.Write(Math.Pow(i,3));
-    else
-    Console.Write($"{Math.Pow(i,3)}, ");
-    }
-}
-
 //Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 //A (3,6,8); B (2,1,-7), -> 15.84
 //A (7,-5, 0); B (1,-1,9) -> 11.53
-//Task21 ();
+Task21 ();
 void Task21 ()
 {
 double[] pointA = new double[3];
@@ -53,14 +36,32 @@ Console.WriteLine("Введите число координату z точки A
 pointA[2] = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("Введите число координату x точки B:");
 pointB[0] = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Введите число координату y точки A:");
+Console.WriteLine("Введите число координату y точки B:");
 pointB[1] = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Введите число координату z точки A:");
+Console.WriteLine("Введите число координату z точки B:");
 pointB[2] = Convert.ToDouble(Console.ReadLine());
 for (int i=0;i<3;i++)
     {
     average = average + Math.Pow((pointA[i]-pointB[i]),2);
     }
 average = Math.Sqrt(average);
-Console.Write(average);
+Console.Write($"Расстояние между точками {average}");
 }
+
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+Task23 ();
+void Task23 ()
+{
+Console.WriteLine("Введите число N для задачи 23:");
+int N = int.Parse(Console.ReadLine());
+for (int i=1;i<=N;i++)
+    {
+    if (i==N)
+    Console.Write(Math.Pow(i,3));
+    else
+    Console.Write($"{Math.Pow(i,3)}, ");
+    }
+}
+
